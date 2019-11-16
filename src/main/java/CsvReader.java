b7f5a1041b51c520794f6246d24bb5119e40c783
yalda <yalda@kako.cc>
2019-11-16 16:23:08 -0600

    init: some more stubbing...

---
+++ src/main/java/CsvReader.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class CsvReader {
+    private CsvReader() {
+
+    }
+    
+    public static final CsvReader from(final File file) {
+        return null;
+    }
+}
--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -7,26 +7,27 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+    public static final String TITLE = "Graffiti Metrics";
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        StackPane root = new StackPane();
+
+        // TODO(@ykako): Set up a little button for now
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
+        btn.setOnAction(event -> System.out.println("Hello World!"));
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setTitle(TITLE);
         primaryStage.show();
     }
 }
