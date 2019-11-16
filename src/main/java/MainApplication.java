c1a1b6b5bcbf592f9f2311528d0cff5f729974fd
yalda <yalda@kako.cc>
2019-11-16 16:11:53 -0600

    init: revert GSON, add city metrics as CSV, clean up some names

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Dashboard extends Application {
+public class MainApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
