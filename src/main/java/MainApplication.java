899d9fb4e7aed642c019c7b6e486630ca4528935
yalda <yalda@kako.cc>
2019-12-05 23:18:36 -0600

    fx: minor nits

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,11 +1,9 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class MainApplication extends Application {
     private static final String TITLE = "Crime Metrics";
