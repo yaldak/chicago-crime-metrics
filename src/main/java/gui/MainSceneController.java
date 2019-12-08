e04251a66f86c5d30217ac241dacf40065090b8a
yalda <yalda@kako.cc>
2019-12-08 15:30:12 -0600

    organize imports

--- src/main/java/gui/MainSceneController.java
+++ src/main/java/gui/MainSceneController.java
@@ -9,10 +9,7 @@ import records.CrimeRecord;
 import util.RecordReader;
 
 import java.io.IOException;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
