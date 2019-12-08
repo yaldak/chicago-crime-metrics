b436d9f7ee7cd6c4ce7794f20243b0ab2d34ed18
yalda <yalda@kako.cc>
2019-12-08 15:45:31 -0600

    data: more charts

--- src/main/java/gui/MainSceneController.java
+++ src/main/java/gui/MainSceneController.java
@@ -9,6 +9,7 @@ import records.CrimeRecord;
 import util.RecordReader;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.Month;
 import java.util.Comparator;
 import java.util.List;
@@ -24,9 +25,17 @@ public class MainSceneController {
     private BarChart<String, Number> crimesByMonthBarChart;
 
     @FXML
+    private BarChart<String, Number> crimesByDayOfMonthBarChart;
+
+    @FXML
+    private BarChart<String, Number> crimesByDayOfWeekBarChart;
+
+    @FXML
     public void initialize() {
         try {
             initializeCrimesByMonthBarChart();
+            initializeCrimesByDayOfMonthBarChart();
+            initializeCrimesByDayOfWeekBarChart();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,5 +62,41 @@ public class MainSceneController {
                         series.getData().add(new XYChart.Data<>(month.toString(), count)));
 
         series.getData().sort(Comparator.comparing(d -> Month.valueOf(d.getXValue())));
+    }
+
+    private void initializeCrimesByDayOfMonthBarChart() throws IOException {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        crimesByDayOfMonthBarChart.setLegendVisible(false);
+        crimesByDayOfMonthBarChart.getData().add(series);
+
+        List<CrimeRecord> crimeRecords = RecordReader.readCrimeRecords();
+
+        crimeRecords.parallelStream()
+                .filter(r -> Objects.nonNull(r.date))
+                .map(r -> r.date.getDayOfMonth())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((dom, count) ->
+                        series.getData().add(new XYChart.Data<>(dom.toString(), count)));
+
+        series.getData().sort(Comparator.comparingInt(d -> Integer.parseInt(d.getXValue())));
+    }
+
+    private void initializeCrimesByDayOfWeekBarChart() throws IOException {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        crimesByDayOfWeekBarChart.setLegendVisible(false);
+        crimesByDayOfWeekBarChart.getData().add(series);
+
+        List<CrimeRecord> crimeRecords = RecordReader.readCrimeRecords();
+
+        crimeRecords.parallelStream()
+                .filter(r -> Objects.nonNull(r.date))
+                .map(r -> r.date.getDayOfWeek())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((dow, count) ->
+                        series.getData().add(new XYChart.Data<>(dow.toString(), count)));
+
+        series.getData().sort(Comparator.comparing(d -> DayOfWeek.valueOf(d.getXValue())));
     }
 }
